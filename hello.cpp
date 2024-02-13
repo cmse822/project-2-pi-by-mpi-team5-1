2da20cfafa905296a8d1afcb240591f253104502
erikazhengyilin <zhengyil@msu.edu>
2024-02-12 22:40:17 -0500

    code for Part 3: MPI Basics

--- hello.cpp
+++ hello.cpp
@@ -10,11 +10,8 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-    if(rank == numtasks-1){
-        cout<<"hello world!"<<endl;
-    }
+    cout<<"hello world!"<< "from process: " << rank << endl;
 
     MPI_Finalize();
-
     return 0;
 }
