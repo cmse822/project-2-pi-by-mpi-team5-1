fc63cfc644650b78cc2cf082faa969c8952d74ee
erikazhengyilin <zhengyil@msu.edu>
2024-02-12 22:55:29 -0500

    q3 update

--- hello.cpp
+++ hello.cpp
@@ -9,8 +9,7 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    cout<<"hello world!"<< "from process: " << rank << endl;
+    cout << "hello world!" << "from process: " << rank << endl;
 
     MPI_Finalize();
     return 0;
